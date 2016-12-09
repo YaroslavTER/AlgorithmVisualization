2ad0da88b5b3532a89338efe036404563d151778
Yaroslav <yaroslavtereshchuk@gmail.com>
2016-12-09 20:42:59 +0200

    set random and normal draw

--- algorithm.js
+++ algorithm.js
@@ -1,15 +1,27 @@
 var canvas = document.getElementById('algorithm_id')
 var ctx = canvas.getContext('2d')
 var color = '#000000'
-var rows = 10, colums = 20
 var side = 20
-var array = [1,2,3,4,5,6,7,8,9]
+var rows = canvas.clientHeight/side, colums = canvas.clientHeight/side
+var array = []
+
+function SetRandom(min, max, length){
+    array = new Array(length)
+    for(var index = 0; index < array.length; index++)
+        array[index]= GetRandomIn(min, max)
+}
+
+function GetRandomIn(min, max){ return Math.floor(Math.random() * (max - min + 1)) + min }
 
 function Draw(){
     ctx.fillStyle = color
     var counter = 1
+    var valueSide
+    var diff;
     for(var index = 0; index < array.length; index++){
-        ctx.fillRect(side*counter, 0, side, array[index]*side)
+        valueSide = array[index]
+        diff=(colums-valueSide)*side
+        ctx.fillRect(side*counter-side, diff, side, valueSide*side)
         counter++
     }
 }
@@ -18,4 +30,5 @@ function Sort(){
 
 }
 
+SetRandom(1,9,40)
 Draw()
