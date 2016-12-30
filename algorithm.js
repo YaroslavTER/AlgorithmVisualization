f4f67ec3925c9b090d8568feba1a1d7bcdf76dce
Yaroslav <yaroslavtereshchuk@gmail.com>
2016-12-30 13:20:00 +0200

    added types of random set

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById('algorithm_id')
 var ctx = canvas.getContext('2d')
 var color = '#000000'
-var side = 20
+var side = 10
 var rows = canvas.clientHeight/side, colums = canvas.clientWidth/side
 var time = 150
 var array = []
@@ -9,7 +9,33 @@ var array = []
 function SetRandom(min, max, length){
     array = new Array(length)
     for(var index = 0; index < array.length; index++)
-        array[index]= GetRandomIn(min, max)
+        array[index] = GetRandomIn(min, max)
+}
+
+function SetRandomPositions(){
+    array = new Array(rows)
+    var length = array.length
+    for(var index = 0; index < length ; index++)
+        array[index] = index + 1
+    Mix()
+    Draw(array)
+}
+
+function Mix(){
+    var length = array.length
+    for(var index = 0; index < length; index++)
+        Swap(index, GetRandomIn(0, length-1))
+}
+
+function Swap(firstPosition, secondPosition){
+    var tmp = array[firstPosition]
+    array[firstPosition] = array[secondPosition]
+    array[secondPosition] = tmp
+}
+
+function SetRandomValues(){
+    SetRandom(1,rows,colums)
+    Draw(array)
 }
 
 function GetRandomIn(min, max){
@@ -72,8 +98,7 @@ function Draw(inputArray){
     }
 }
 
-SetRandom(1,rows,colums)
-Draw(array)
+SetRandomValues()
 
 document.addEventListener('keydown', function(event) {
     if(event.which == 13 || event.which == 32)
